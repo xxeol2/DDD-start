0bf0fc8605943a6128a890a2cedecb7e1cbc7c45
xxeol2 <jhj01177@naver.com>
2022-11-13 23:03:34 +0900

    [1.4] 배송지 변경 가능 여부 판단 기능 Order에 있는 경우

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -3,28 +3,18 @@ public class Order {
 	private ShippingInfo shippingInfo;
 
 	public void changeShippingInfo(ShippingInfo newShippingInfo) {
-		if (!state.isShippingChangeable()) {
+		if (!isShippingChangeable()) {
 			throw new IllegalStateException("can't change shipping in " + state);
 		}
 		this.shippingInfo = newShippingInfo;
 	}
+
+	private boolean isShippingChangeable() {
+		return state == OrderState.PAYMENT_WAITING || state == OrderState.PREPARING;
+	}
 	// ...
 
 	public enum OrderState {
-		PAYMENT_WAITING {
-			public boolean isShippingChangeable() {
-				return true;
-			}
-		},
-		PREPARING {
-			public boolean isShippingChangeable() {
-				return true;
-			}
-		},
-		SHIPPED, DELIVERING, DELIVERY_COMPLETED;
-
-		public boolean isShippingChangeable() {
-			return false;
-		}
+		PAYMENT_WAITING, PREPARING, SHIPPED, DELIVERING, DELIVERY_COMPLETED;
 	}
 }
