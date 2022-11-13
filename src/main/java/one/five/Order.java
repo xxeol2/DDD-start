f619c5a40756e97e9aaf23b533d151fc252ece85
xxeol2 <jhj01177@naver.com>
2022-11-13 23:34:28 +0900

    [1.5] 배송지 정보 (ShippingIfno)

--- src/main/java/one/five/Order.java
+++ src/main/java/one/five/Order.java
@@ -4,16 +4,25 @@ import java.util.List;
 
 public class Order {
 	private List<OrderLine> orderLines;
+	private ShippingInfo shippingInfo;
 	private Money totalAmounts;
 
-	public Order(List<OrderLine> orderLines) {
+	public Order(List<OrderLine> orderLines, ShippingInfo shippingInfo) {
 		setOrderLines(orderLines);
+		setShippingInfo();
 	}
 
 	private void setOrderLines(List<OrderLine> orderLines) {
 		verifyAtLeastOneOrMoreOrderLines(orderLines);
 		this.orderLines = orderLines;
 		calculateTotalAmounts();
+	}
+
+	private void setShippingInfo(ShippingInfo shippingInfo) {
+		if (shippingInfo == null) {
+			throw new IllegalArgumentException("no ShippingInfo");
+		}
+		this.shippingInfo = shippingInfo;
 	}
 
 	private void verifyAtLeastOneOrMoreOrderLines(List<OrderLine> orderLines) {
