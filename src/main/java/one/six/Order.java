434886efcf4068a0a04d11b77ec67ac9feb36ec0
xxeol2 <jhj01177@naver.com>
2022-12-12 16:50:17 +0900

    [1.6] 도메인 모델에 set메서드 넣지 않기

--- src/main/java/one/six/Order.java
+++ src/main/java/one/six/Order.java
@@ -1,8 +1,50 @@
 package one.six;
 
+import java.util.List;
+import one.five.OrderState;
+
 public class Order {
 
     private OrderNo id;
+    private Orderer orderer;
+    private List<OrderLine> orderLines;
+    private ShippingInfo shippingInfo;
+    private OrderState orderState;
+    private Money totalAmounts;
+
+
+    public Order(Orderer orderer, List<OrderLine> orderLines, ShippingInfo shippingInfo,
+            OrderState orderState) {
+        setOrderer(orderer);
+        setOrderLines(orderLines);
+        // ...
+    }
+
+    private void setOrderer(Orderer orderer) {
+        if (orderer == null) {
+            throw new IllegalArgumentException("no orderer");
+        }
+        this.orderer = orderer;
+    }
+
+    private void setOrderLines(List<OrderLine> orderLines) {
+        verifyAtLeastOneOrMoreOrderLines(orderLines);
+        this.orderLines = orderLines;
+        calculateTotalAmounts();
+    }
+
+    private void verifyAtLeastOneOrMoreOrderLines(List<OrderLine> orderLines) {
+        if (orderLines == null || orderLines.isEmpty()) {
+            throw new IllegalArgumentException("no OrderLine");
+        }
+    }
+
+    private void calculateTotalAmounts() {
+        int sum = orderLines.stream()
+                .mapToInt(x -> x.getAmounts())
+                .sum();
+        this.totalAmounts = new Money(sum);
+    }
 
     public OrderNo getId() {
         return id;
